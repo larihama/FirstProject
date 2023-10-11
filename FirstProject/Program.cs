da427bc2fe0a419033c650b60facb254fa6485c3
finkk <finkk@BOOK-G2J3M62TJR>
2023-10-11 08:45:20 +0300

    Add project files.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace FirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tehtävä 1: Tulosta "Hello World!"
+            Console.WriteLine("Hello World!");
+
+            //Tehtävä 2: Kysy käyttäjältä nimi
+            Console.WriteLine("Syötä etunimesi:");
+            var name = Console.ReadLine();
+
+            //Tehtävä 3: Poista nimestä turhat välilyönnit
+            name = name.Replace(" ", "");
+
+            //Tehtävä 4: Muuta nimen ensimmäinen kirjain isoksi ja muut pieniksi käyttämällä CapitalizeFirstLetter funktiota
+            name = name.First().ToString().ToUpper() + name.Substring(1).ToLower();
+
+            //ulosta "Terve, <name>!"
+            Console.WriteLine($"Terve {name}!");
+
+            //Tehtävä 5: Kysy käyttäjältä sukunimi omaan muuttujaan ja tulosta "Terve, <firstName> <lastName>!"
+            Console.WriteLine("Syötä sukunimesi:");
+            var lastName = Console.ReadLine();
+            lastName = lastName.Replace(" ", "");
+            lastName = lastName.First().ToString().ToUpper() + lastName.Substring(1).ToLower();
+            Console.WriteLine("Terve " + name + " " + lastName + "!");
+
+            //Tulosta käyttäjän nimi kolmella eri tavalla, käyttäen eri string tekniikoita
+            //Katso esimerkkiä SecondProject\Program.cs
+            Console.WriteLine(string.Format("Nimesi on {0} {1}", name, lastName));
+            Console.WriteLine($"Eikö nimesi ole {name} {lastName}?");
+
+            //Tehtävä 6: Kommentoi koodi selventämään, että mitä se tekee
+
+        }
+
+        private static string CapitalizeFirstLetter(string? inputString)
+        {
+            /*inputString.First().ToString().ToUpper(): Ottaa ensimmäisen merkin inputString-merkkijonosta, 
+             * muuttaa sen takaisin merkkijonoksi char tyyppisestä muuttujasta ja muuttaa sen isoksi kirjaimeksi (eli alkukirjaimeksi).
+
+             inputString.Substring(1).ToLower(): Ottaa inputString-merkkijonon loput merkit ensimmäisen merkin jälkeen ja muuttaa ne pieniksi kirjaimiksi.
+             Lopuksi yllä mainitut kaksi osaa yhdistetään yhteen merkkijonoon käyttäen + -operaattoria.
+             Koodin tulos on, että inputString-merkkijonon ensimmäinen kirjain muutetaan isoksi kirjaimeksi ja kaikki muut merkit pieniksi kirjaimiksi. 
+             Esimerkiksi jos inputString oli alun perin "hELLo", sen arvo koodin suorittamisen jälkeen on "Hello".*/
+
+            inputString = inputString.First().ToString().ToUpper() + inputString.Substring(1).ToLower();
+            return inputString;
+        }
+
+
+    }
+}
---
+++ SecondProject/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace SecondProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Moro moro, lasketaan kahdella luvulla toimituksia");
+            Console.WriteLine("Syötä x");
+
+            var x = Console.ReadLine();
+
+            Console.WriteLine("Syötä y");
+
+            var y = Console.ReadLine();
+
+            //Tehtävä 7: Kysy käyttäjältä laskuoperaatio +,-, * tai / ja tulosta laskun tulos
+
+            Console.WriteLine("Syötä laskuoperaatio +, -, * tai /:");
+
+            var operaatio = Console.ReadLine();
+
+            int result = 0;
+
+            if (operaatio == "+")
+            {
+                result = int.Parse(x) + int.Parse(y);
+            }
+            else if (operaatio == "-")
+            {
+                result = int.Parse(x) - int.Parse(y);
+            }
+            else if (operaatio == "*")
+            {
+                result = int.Parse(x) * int.Parse(y);
+            }
+            else if (operaatio == "/")
+            {
+                result = int.Parse(x) / int.Parse(y);
+            }
+
+            Console.WriteLine($"Tulos on {result} hYVÄÄÄ");
+            Console.WriteLine("Mees ny töihi siitä");
+
+            //parses string to int and adds them together
+            //var result = int.Parse(x) + int.Parse(y);
+
+
+            // Console.WriteLine("Combine result + is " + result);
+            // Console.WriteLine(string.Format("String format result is {0}", result));
+            // Console.WriteLine($"Interpolation result $ is {result}");
+
+
+            // Console.WriteLine("Combine example: Result " + x + " +  " + y + " = " + result);
+            // Console.WriteLine(string.Format("string.Format example: Result {0} + {1} = {2}", x, y, result));
+            // Console.WriteLine($"Interpolation example: Result {x} + {y} = {result}");
+        }
+    }
+}
